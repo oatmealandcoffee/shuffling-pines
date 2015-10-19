69e57085bc9cabd4106622e566f17b002d055c0c
Philip Regan <grainsandbeans@oatmealandcoffee.com>
2015-10-19 18:07:26 -0400

    Added Create Test

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,36 +1,36 @@
 describe("Shuffling Pines CRUD", function() {
 
-    var store;
+    var store = [
+        {
+            fname:"Tyler",
+            lname:"Durden",
+            txdate:"2015/10/18",
+            status:"pick up",
+            loc:"Boston"
+        },
+        {
+            fname:"Marla",
+            lname:"Singer",
+            txdate:"2015/10/18",
+            status:"arrived",
+            loc:"Boston"
+        },
+        {
+            fname:"Robert",
+            lname:"Paulson",
+            txdate:"2015/10/18",
+            status:"drop off",
+            loc:"Boston"
+        }
+    ];
 
     beforeEach(function() {
-        store = [
-            {
-                fname:"Tyler",
-                lname:"Durden",
-                txdate:"2015/10/18",
-                status:"pick up",
-                loc:"Boston"
-            },
-            {
-                fname:"Marla",
-                lname:"Singer",
-                txdate:"2015/10/18",
-                status:"arrived",
-                loc:"Boston"
-            },
-            {
-                fname:"Robert",
-                lname:"Paulson",
-                txdate:"2015/10/18",
-                status:"drop off",
-                loc:"Boston"
-            }
-        ];
 
         // Create, Update
-        spyOn(localStorage, "setItem").and.callFake( function(key, value) {
+        spyOn(localStorage, "setItem").and.callFake( function( key, value) {
             store[key] = value + '';
         });
+
         // Retrieve, Update
         spyOn(localStorage, "getItem").and.callFake( function(key) {
             return store[key] || '[]';
@@ -44,7 +44,21 @@ describe("Shuffling Pines CRUD", function() {
     });
 
     describe("Create Suite", function() {
+        it('should add a new person', function () {
 
+            var person = {
+                fname:"Raymond",
+                lname:"Kessel",
+                txdate:"2015/10/18",
+                status:"drop off",
+                loc:"Boston"
+            };
+            var preLen = store.length;
+            localStorage.setItem(preLen, person);
+            var postLen = store.length;
+            expect( preLen ).toBeLessThan( postLen );
+
+            });
     });
 
     describe("Retrieve Suite", function() {
